61dbe1516b283637f301fedfe9e8b2638fbadb14
suwashimizu <suwawa2013@gmail.com>
2018-10-06 01:48:48 +0900

    add ISearchEventView

--- app/src/main/java/org/suwashizmu/connpassapp/module/presenter/EventSearchPresenter.kt
+++ app/src/main/java/org/suwashizmu/connpassapp/module/presenter/EventSearchPresenter.kt
@@ -1,18 +1,21 @@
 package org.suwashizmu.connpassapp.module.presenter
 
+import com.orhanobut.logger.Logger
 import org.suwashizmu.connpassapp.module.output.EventSearchOutputData
+import org.suwashizmu.connpassapp.module.view.ISearchEventView
 import org.suwashizmu.connpassapp.module.view.SearchEventViewModel
 
 /**
  * Created by KEKE on 2018/10/05.
  */
-class EventSearchPresenter : IEventSearchPresenter {
+class EventSearchPresenter(private val view: ISearchEventView) : IEventSearchPresenter {
 
     override fun complete(outputData: EventSearchOutputData) {
         val viewModel = SearchEventViewModel(outputData.eventList.map {
             SearchEventViewModel.Event(it.title)
         })
 
-        print(viewModel)
+        Logger.d(viewModel)
+        view.updated(viewModel)
     }
 }
---
+++ app/src/main/java/org/suwashizmu/connpassapp/module/view/ISearchEventView.kt
@@ -0,0 +1,8 @@
+package org.suwashizmu.connpassapp.module.view
+
+/**
+ * Created by KEKE on 2018/10/06.
+ */
+interface ISearchEventView {
+    fun updated(viewModel: SearchEventViewModel)
+}
