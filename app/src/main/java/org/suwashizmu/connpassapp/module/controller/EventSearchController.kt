8b3c4c76a823be4caaec90033a79c266f5bd6a05
suwashimizu <suwawa2013@gmail.com>
2018-10-06 01:26:07 +0900

    add EventSearchControllerTest

---
+++ app/src/main/java/org/suwashizmu/connpassapp/module/controller/EventSearchController.kt
@@ -0,0 +1,18 @@
+package org.suwashizmu.connpassapp.module.controller
+
+import org.suwashizmu.connpassapp.module.input.EventSearchInputData
+import org.suwashizmu.connpassapp.module.usecase.IEventSearchUseCase
+
+/**
+ * Created by KEKE on 2018/10/06.
+ */
+class EventSearchController(private val eventSearchUseCase: IEventSearchUseCase) {
+
+    fun eventSearch(vararg keyword: String) {
+        val inputData = EventSearchInputData(
+                keyword = keyword.toSet()
+        )
+
+        eventSearchUseCase.search(inputData)
+    }
+}
