4924ec00296514fd3b75fbb1a8152a951cc7f774
suwashimizu <suwawa2013@gmail.com>
2018-10-05 23:53:57 +0900

    add RemoteEventDataSourceTest

--- app/src/main/java/org/suwashizmu/connpassapp/repository/RemoteEventDataSource.kt
+++ app/src/main/java/org/suwashizmu/connpassapp/repository/RemoteEventDataSource.kt
@@ -3,16 +3,15 @@ package org.suwashizmu.connpassapp.repository
 import io.reactivex.Single
 import org.suwashizmu.connpassapp.entity.Event
 import org.suwashizmu.connpassapp.mapper.EventMapper
-import org.suwashizmu.connpassapp.service.api.ConnpassClient
+import org.suwashizmu.connpassapp.service.api.ConnpassService
 import org.suwashizmu.connpassapp.service.api.SearchQuery
 
 /**
  * Created by KEKE on 2018/10/05.
  * api経由でEventを取得する
  */
-class RemoteEventDataSource : EventRepository {
+class RemoteEventDataSource(private val client: ConnpassService) : EventRepository {
 
-    private val client = ConnpassClient.service
     private val mapper = EventMapper()
 
     override fun findEvent(vararg keyword: String): Single<Collection<Event>> {
--- app/src/test/java/org/suwashizmu/connpassapp/repository/RemoteEventDataSourceTest.kt
+++ app/src/test/java/org/suwashizmu/connpassapp/repository/RemoteEventDataSourceTest.kt
@@ -1,14 +1,30 @@
 package org.suwashizmu.connpassapp.repository
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import io.reactivex.Single
 import org.junit.Test
+import org.suwashizmu.connpassapp.service.api.ConnpassService
+import org.suwashizmu.connpassapp.service.api.SearchResult
 
 /**
  * Created by KEKE on 2018/10/05.
  */
 class RemoteEventDataSourceTest {
 
+    private val mock: ConnpassService = mock {
+        //mapperでヌルポが出るのでデータを返すようにしている
+        //mapperのテストが出来ていれば確認することはメソッドのコールのみである
+        on { searchEvents(any()) } doReturn Single.just(SearchResult(1, emptyList(), 2, 3))
+    }
+    private val remoteDataSource = RemoteEventDataSource(mock)
+
     @Test
     fun `find event`() {
+        remoteDataSource.findEvent("kotlin")
 
+        verify(mock).searchEvents(any())
     }
 }
