955f1d1780d2eab01678c9fa626240d145ba7686
suwashimizu <suwawa2013@gmail.com>
2018-10-08 01:25:13 +0900

    add InterestCategory

---
+++ app/src/main/java/org/suwashizmu/connpassapp/module/entity/InterestCategory.kt
@@ -0,0 +1,27 @@
+package org.suwashizmu.connpassapp.module.entity
+
+/**
+ * Created by KEKE on 2018/10/08.
+ * 興味のある分野の一覧、どんどん増やすよ
+ */
+enum class InterestCategory(val id: Int) {
+    KOTLIN(1),
+    JAVA(2),
+    PYTHON(3),
+    RUBY(4),
+    MACHINE_LEARNING(5),
+    AI(6),
+    ARCHITECTURE(7),
+    DESIGN(8),
+    JAVA_SCRIPT(9),
+    C_SHAPE(10),
+    UNITY(11),
+    INFRASTRUCTURE(12),
+    SWIFT(13),
+    ANDROID(14),
+    I_OS(15),
+    EVENT(16),
+    ELIXIR(17),
+    UX(18),
+    SCALA(19)
+}
