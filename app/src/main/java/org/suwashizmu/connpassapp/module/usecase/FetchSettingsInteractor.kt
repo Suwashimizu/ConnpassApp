fe0d7a9f537eeb03628acbf70942fd3825735f36
suwashimizu <suwawa2013@gmail.com>
2018-10-18 22:35:03 +0900

    add SettingsOutputPort

--- app/src/main/java/org/suwashizmu/connpassapp/module/usecase/FetchSettingsInteractor.kt
+++ app/src/main/java/org/suwashizmu/connpassapp/module/usecase/FetchSettingsInteractor.kt
@@ -1,16 +1,17 @@
 package org.suwashizmu.connpassapp.module.usecase
 
+import org.suwashizmu.connpassapp.module.output.SettingsOutputPort
 import org.suwashizmu.connpassapp.module.repository.AreaRepository
 import org.suwashizmu.connpassapp.module.repository.InterestCategoryRepository
 
 /**
  * Created by KEKE on 2018/10/18.
  */
-class FetchSettingsInteractor(private val areaRepository: AreaRepository,
+class FetchSettingsInteractor(private val settingsOutputPort: SettingsOutputPort,
+                              private val areaRepository: AreaRepository,
                               private val interestCategoryRepository: InterestCategoryRepository) : IFetchSettings {
 
     override fun fetchSettings() {
-        areaRepository.getArea()
-        interestCategoryRepository.getCurrentInterestCategories()
+        settingsOutputPort.complete(areaRepository.getArea(), interestCategoryRepository.getCurrentInterestCategories())
     }
 }
