0fadbf6c1ec60624d7f3610cf6bf019d9f5ac435
suwashimizu <suwawa2013@gmail.com>
2018-11-17 22:28:18 +0900

    add show eventPage with CustomsTab

---
+++ app/src/main/java/org/suwashizmu/connpassapp/module/repository/StubEventDataSource.kt
@@ -0,0 +1,36 @@
+package org.suwashizmu.connpassapp.module.repository
+
+import io.reactivex.Single
+import org.suwashizmu.connpassapp.module.entity.Area
+import org.suwashizmu.connpassapp.module.entity.Event
+import org.suwashizmu.connpassapp.module.entity.EventList
+import org.threeten.bp.ZonedDateTime
+
+/**
+ * Created by KEKE on 2018/11/17.
+ */
+class StubEventDataSource : EventRepository {
+
+    private val event = Event(
+            id = 106516,
+            title = "第6回 意思決定のためのデータ分析勉強会 in Fukuoka",
+            catch = "〜 異常検知、品質向上・改善祭り 〜",
+            description = "description",
+            ownerName = "doradora09",
+            eventUtl = "https://ishikettei.connpass.com/event/106516/",
+            twitterHashTag = "ishikettei",
+            startedAt = ZonedDateTime.parse("2018-11-17T14:00+09:00"),
+            endedAt = ZonedDateTime.parse("2018-11-17T18:00+09:00"),
+            limit = 46,
+            place = "福岡システムＬＳＩ総合開発センター (４階　交流サロン)",
+            address = "〒814-0001 福岡県福岡市早良区百道浜３丁目８−３３",
+            accepted = 41,
+            waiting = 0
+    )
+
+    override fun findById(id: Int): Single<Event> =
+            Single.just(event)
+
+    override fun findEventList(start: Int, limit: Int, area: Area?, vararg keywordOr: String): Single<EventList> =
+            Single.just(EventList(1, listOf(event)))
+}
--- app/src/main/java/org/suwashizmu/connpassapp/view/EventDetailsFragment.kt
+++ app/src/main/java/org/suwashizmu/connpassapp/view/EventDetailsFragment.kt
@@ -1,6 +1,9 @@
 package org.suwashizmu.connpassapp.view
 
+import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,6 +12,8 @@ import android.view.ViewGroup
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.orhanobut.logger.Logger
@@ -46,11 +51,6 @@ class EventDetailsFragment : Fragment(), IEventDetailsView {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        //CustomTabを使おう!
-        //https://techbooster.org/android/application/17097/
-        binding.webView.webViewClient = webViewClient
-        binding.webView.settings.javaScriptEnabled = true
-
         presenter?.onCreate()
         presenter?.getEvent(getEventId())
     }
@@ -85,7 +85,29 @@ class EventDetailsFragment : Fragment(), IEventDetailsView {
 
     override fun update(viewModel: EventDetailsViewModel) {
         if (viewModel.error == null) {
-            binding.webView.loadUrl(viewModel.eventUrl)
+
+            //URL共有を追加
+            val shareIntent = Intent(Intent.ACTION_SEND)
+                    .setType("text/plain")
+                    .putExtra(Intent.EXTRA_TEXT, viewModel.eventUrl)
+            val pendingIntent = PendingIntent.getActivity(requireContext(), 0, shareIntent, 0)
+            val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_share_black_24dp)
+                    ?: return
+
+            //vectorからbitmapを生成
+            val icon = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(icon)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+
+            //ChromeTabsの設定
+            val tabsIntent = CustomTabsIntent.Builder()
+                    .setShowTitle(true)
+                    .setToolbarColor(ContextCompat.getColor(requireActivity(), R.color.colorPrimary))
+                    .setActionButton(icon, getString(R.string.share), pendingIntent)
+                    .build()
+
+            tabsIntent.launchUrl(requireActivity(), Uri.parse(viewModel.eventUrl))
         }
 
         Logger.d(viewModel)
